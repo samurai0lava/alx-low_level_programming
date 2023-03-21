54a1f3fea97145fc88fad118c2bcbd39fe53dada
samurai0lava <ouhsseineilyass@gmail.com>
2023-03-21 13:49:24 -0700

    add

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+*main prints 0-putchar
+*returns 0 always
+*/
+int main (){
+printf("-putchar\n");
+return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0x02. C - Functions, nested loops
+task 0: 0. _putchar prints out "_putchar"
