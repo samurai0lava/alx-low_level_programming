dd5f6d28332ef821892b5aa956970671d303c0dd
samurai0lava <ouhsseineilyass@gmail.com>
2023-04-04 13:52:01 -0700

    add

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,18 @@
  * in the string @s, or NULL if the character is not found
  */
 
-char *_strchr(char *s, char c){
-    
-    int i;
-    
-    for(i=0; s[i] != '\0'; i++){
-        
-        if(s[i] == c)
-        {
-            return (s+i);
-        }
-    }
-    
-    return (NULL);
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
 }
 
