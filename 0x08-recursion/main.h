5b0917474482a76396d963f8823571a16807667c
samurai0lava <ouhsseineilyass@gmail.com>
2023-04-05 03:03:12 -0700

    main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+#endif/*MAIN_H*/
+
+
