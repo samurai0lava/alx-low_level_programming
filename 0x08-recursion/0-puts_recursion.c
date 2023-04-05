5bd264e6c996f25f1d193eae6dc676dd849e2970
samurai0lava <ouhsseineilyass@gmail.com>
2023-04-05 02:50:28 -0700

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - function prints a string, followed by a new line
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+    puts (s);
+}
